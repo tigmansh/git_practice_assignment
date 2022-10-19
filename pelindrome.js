46a05c86d5677040e0f648c1f703946a1c8f1550
roy <roy19rishabh@gmail.com>
2022-10-19 15:22:05 +0530

    Code errors resolved

--- pelindrome.js
+++ pelindrome.js
@@ -1,12 +1,12 @@
 let str = "aba";
 let bag = "";
 
-for (let i = str.length; i >= 0; i--) {
+for (let i = str.length-1; i >= 0; i--) {
   bag += str[i];
 }
 
 if (str == bag) {
-  console.log(str + " is a pallindrome");
+  console.log(str + " is a palindrome");
 } else {
-  console.log(str + " is not a pallindrome");
+  console.log(str + " is not a palindrome");
 }
